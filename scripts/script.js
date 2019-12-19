38890a10d60f714964dd756d054129d223822a10
Evgeniy Strunevskiy <esstunevskiy@gmail.com>
2019-12-19 17:18:08 +0300

    26 commit

--- scripts/script.js
+++ scripts/script.js
@@ -180,62 +180,66 @@ function ready() {
   };
 
 
+  //Карта
+  
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+      center: [52.09058992970443,23.70227846134666],
+      zoom: 9
+    },{
+      searchControlProvider: 'yandex#search'
+    }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        hintContent: 'Собственный значок метки',
+        balloonContent: 'Это красивая метка'
+      }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#image',
+        // Своё изображение иконки метки.
+        iconImageHref: '../pic/icons/map-marker.svg',
+        // Размеры метки.
+        iconImageSize: [30, 42],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-5, -38]
+      }),
+
+      myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
+        hintContent: 'Собственный значок метки с контентом',
+        balloonContent: 'А эта — новогодняя',
+        iconContent: '12'
+      }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#imageWithContent',
+        // Своё изображение иконки метки.
+        iconImageHref: '../pic/icons/map-marker.svg',
+        // Размеры метки.
+        iconImageSize: [48, 48],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-24, -24],
+        // Смещение слоя с содержимым относительно слоя с картинкой.
+        iconContentOffset: [15, 15],
+        // Макет содержимого.
+        iconContentLayout: MyIconContentLayout
+      });
+
+    myMap.geoObjects
+      .add(myPlacemark)
+      .add(myPlacemarkWithContent);
+      myMap.behaviors.disable('scrollZoom');
+  });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // const right = document.querySelector(".slider__arrow-right");
-  // const left = document.querySelector(".slider__arrow-left");
-  // const sliderList = document.querySelector(".slider__list");
-  // const computed = getComputedStyle(sliderList);
-  // console.log(computed);
-
-  // right.addEventListener("click", function (e) {
-  //   e.preventDefault();
-  //   let currentRight = parseInt(computed.right);
-
-  //   if (!currentRight) {
-  //     currentRight = 0;
-  //   }
-
-  //   if (currentRight < 3760) {
-  //     sliderList.style.right = currentRight + 940 + 'px';
-  //   }
-  // });
-
-  // left.addEventListener("click", function (e) {
-  //   e.preventDefault();
-  //   let currentRight = parseInt(computed.right);
-
-  //   if (!currentRight) {
-  //     currentRight = 0;
-  //   }
-
-  //   if (currentRight > 0) {
-  //     sliderList.style.right = currentRight - 940 + 'px';
-  //   }
-  // });
-
-
-  // if (direction === "right") {
-  //   sliderList.appendChild(sliderList.firstChild);
-  // } else {
-  //   sliderList.insertBefore(sliderList.lastElementChild, sliderList.firstElementChild);
-  // }
-  // }
 
 
 };
