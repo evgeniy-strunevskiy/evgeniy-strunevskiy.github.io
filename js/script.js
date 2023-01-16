ca0c0407fa42965e8860ec171e51df08013472ed
Evgeniy Strunevskiy <esstrunevskiy@gmail.com>
2023-01-16 12:26:48 +0300

    skills

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 const menuLinks = document.querySelectorAll(".menu_link[data-goto]");
 
-console.log(menuLinks)
+console.log(menuLinks);
 if (menuLinks.length > 0) {
   menuLinks.forEach((menuLink) => {
     menuLink.addEventListener("click", onMenuLinkClick);
   });
 
   function onMenuLinkClick(e) {
-    e.preventDefault()
     const menuLink = e.target;
     if (
       menuLink.dataset.goto &&
@@ -16,27 +15,29 @@ if (menuLinks.length > 0) {
       const gotoBlock = document.querySelector(menuLink.dataset.goto);
       const gotoBlockValue =
         gotoBlock.getBoundingClientRect().top +
-        scrollX -
+        scrollY -
         document.querySelector("header").offsetHeight;
-        if(iconMenu.classList.contains('_active')) {
-          document.body.classList.remove('_lock')
-          iconMenu.classList.remove('_active');
-          menuBody.classList.remove('_active');
-        }
+      console.log(gotoBlock.getBoundingClientRect().top);
+      if (iconMenu.classList.contains("_active")) {
+        document.body.classList.remove("_lock");
+        iconMenu.classList.remove("_active");
+        menuBody.classList.remove("_active");
+      }
       window.scrollTo({
         top: gotoBlockValue,
         behavior: "smooth",
       });
+      e.preventDefault();
     }
   }
 }
 
-const iconMenu = document.querySelector('.menu_icon');
-const menuBody = document.querySelector('.menu_body');
+const iconMenu = document.querySelector(".menu_icon");
+const menuBody = document.querySelector(".menu_body");
 if (iconMenu) {
-  iconMenu.addEventListener('click', function(e) {
-    document.body.classList.toggle('_lock')
-    iconMenu.classList.toggle('_active');
-    menuBody.classList.toggle('_active');
-  })
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
 }
